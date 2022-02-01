d16a9f67c9c7dee5a3f0ce1a40f31058c7c73567
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-01-31 16:54:10 -0800

    created appJS to write to only new files

--- classwork/intro-to-node/app.js
+++ classwork/intro-to-node/app.js
@@ -1,1 +1,20 @@
-console.log("Getting started with Node.js");
+// console.log("Getting started with Node.js");
+
+const fs = require("fs");
+const content = "You are awesome!";
+
+const prompt = require("prompt");
+prompt.start();
+
+prompt.get(['filename'], function (err, result) {
+  
+  if (fs.existsSync(result.filename)) {
+    console.log("Please give a new filename. That already exist!");
+
+  } else {
+    console.log('File-Name: ' + result.filename);
+    fs.writeFileSync(result.filename, content);
+    console.log("File written successfuly");
+  }
+});
+
